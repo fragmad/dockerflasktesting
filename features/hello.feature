d43d1cc93ab894337f52a35d1050a23cc1b77a87
Will Ellwood <fragmad@gmail.com>
2017-04-02 19:13:19 +0100

    Mangled feature

--- features/hello.feature
+++ features/hello.feature
@@ -1,12 +1,21 @@
 Feature: Hello is an application which says hello in various ways.
 
-    Scenario: Hello World! 
+    Scenario: Hello World!
     Given Hello is set up
         When we connect to "/"
         Then we should see a message saying "Hello World"
 
     Scenario: Hello to a name
-       Given Hello is set up 
+       Given Hello is set up
        When we connect to "/hello/Jane%20Doe"
        Then we should see a message saying "Hello Jane Doe"
 
+    Scenario: JSON List of Names Sent
+      Given Hello is set up
+      Given a list of Names
+      | Names   |
+      | Will    |
+      | Jenny   |
+      | Deckard |
+      When we send that list to "/hello"
+      Then we should see Hello Message for each name.
--- features/steps/hello_steps.py
+++ features/steps/hello_steps.py
@@ -1,3 +1,4 @@
+import json
 from behave import *
 
 @given(u'Hello is set up')
@@ -22,3 +23,20 @@ def step_impl(context, name):
 @then(u'we should see a message saying "{greeting}"')
 def step_impl(context, greeting):
     assert greeting in context.page.data
+
+@given(u'a list of Names')
+def step_impl(context):
+    context.names = []
+    for name in context.table:
+        context.names.append(name[0])
+
+@when(u'we send that list to "/hello"')
+def step_impl(context):
+    data = {'names' : context.names}
+    data_json = json.dumps(data)
+    context.page = context.client.post('/hello', data=data_json)
+    assert context.client
+    
+@then(u'we should see Hello Message for each name.')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we should see Hello Message for each name.')
